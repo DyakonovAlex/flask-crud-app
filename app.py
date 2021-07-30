bded7a43862fbcf6b76342c3d17bc70172245c6c
DyakonovAlex <79167505551@yandex.ru>
2021-07-30 09:21:36 +0300

    Add database

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 import os
+
 from flask import Flask, render_template
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 env_config = os.getenv("APP_SETTINGS", "config.DevelopmentConfig")
 app.config.from_object(env_config)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Data
 
 
 @app.route('/')
--- config.py
+++ config.py
@@ -4,7 +4,9 @@ import os
 class Config:
     DEBUG = False
     DEVELOPMENT = False
+    CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql:///crud")
 
 
 class ProductionConfig(Config):
