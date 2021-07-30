d0207239144e9b8086c5e70efec566286ed4c290
DyakonovAlex <79167505551@yandex.ru>
2021-07-30 09:49:02 +0300

    Fix error in Heroku Postgres

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ class Config:
     CSRF_ENABLED = True
     SECRET_KEY = os.getenv("SECRET_KEY", "this-is-the-default-key")
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql:///crud")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 
 
 class ProductionConfig(Config):
